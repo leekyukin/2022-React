e98fa636203226ed2976e19b9026958b14b656ef
leekyukin <leekujin14@gmail.com>
2022-03-22 19:28:12 +0900

    2022-03-22 Using object

---
+++ day02/introduce-myself/index.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "리죄명",
+    age: "1",
+    id: "1"
+}
+
+function logData() {
+    alert("저는 " + person.id + " " + person.name + "이고 나이는 " + person.age+"입니다")
+}
+
+logData();
