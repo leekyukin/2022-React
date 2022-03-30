f17a4fa056080ffe9b604e4ecb843d4d103efd32
leekyukin <leekujin14@gmail.com>
2022-03-30 19:28:01 +0900

    2022-03-30

--- day03/generatePw/index.js
+++ day03/generatePw/index.js
@@ -4,17 +4,11 @@ const pwList = document.getElementById("pw-list");
 const generateBtn = document.getElementById("generate-btn");
 
 
-function generate() {
-
-    
-}
 
 generateBtn.addEventListener( "click", function ()  {
     for(let i = 0; i < 4; i++) {
         let pwLength = Math.floor(Math.random() * 17)  + 8;
         var pw = [];
-
-        console.log(pwLength + 'sdf');
 
         for(let j = 0; j < pwLength; j++) {
             let n = Math.floor(Math.random() * chars.length) ;
@@ -28,7 +22,7 @@ generateBtn.addEventListener( "click", function ()  {
         }
 
         pwList.innerHTML += `
-            <li id="pw${i + 1}">${result}</li>
+            <input type="button" id="pw${i}" onclick="getResult(this.id)" value="${result}">
         `;
 
         console.log(pwList);
@@ -36,17 +30,10 @@ generateBtn.addEventListener( "click", function ()  {
     }
 });
 
-let pw1 = document.getElementById("pw1");
-let pw2 = document.getElementById("pw2");
-let pw3 = document.getElementById("pw3");
-let pw4 = document.getElementById("pw4");
+
 let resultEl = document.getElementById("result-el");
 
-function getResult() {
-    let result = pw1.value;
-    resultEl.innerText = result;
+function getResult(resultId) {
+    resultEl.innerText = resultId.value;
 }
 
-
-pw1.addEventListener("click", getResult());
-pw
