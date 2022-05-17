9bb04ab6c484f6a18146deca4036ae8a2d72f34a
leekyukin <leekujin14@gmail.com>
2022-05-17 11:05:16 +0900

    ADD :: 입문006 생명주기 함수 static getDerivedStateFromProps(props, state) 사용하기

--- day12/my-app/src/App.js
+++ day12/my-app/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import R005_LifecycleEx from './component/R005_LifecycleEx';
+import LifecycleEx from './component/R006_LifecycleEx';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <R005_LifecycleEx/>
+      <LifecycleEx
+      props_value = 'FromApp.js'
+      />
     </div>
   );
 }
