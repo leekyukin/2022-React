762125da4da61bf3d23a31e00ce7a2632a2d1699
leekyukin <leekujin14@gmail.com>
2022-05-17 11:18:42 +0900

    ADD :: 입문007 생명주기 함수 componentDidMount()

--- day12/my-app/src/App.js
+++ day12/my-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import LifecycleEx from './component/R006_LifecycleEx';
+import LifecycleEx from './component/R007_LifecycleEx';
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
       <LifecycleEx
-      props_value = 'FromApp.js'
+      prop_value = 'FromApp.js'
       />
     </div>
   );
